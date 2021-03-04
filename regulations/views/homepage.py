d3c3f741c0f93bae84e77d109319d61821757ab4
Caleb Godwin <cgodwin@fearless.tech>
2021-03-04 12:30:00 -0500

    Added main header to HomepageView

--- regulations/views/homepage.py
+++ regulations/views/homepage.py
@@ -1,4 +1,9 @@
+from datetime import datetime
+
 from django.views.generic.base import TemplateView
+
+from regulations.views import utils
+from regulations.generator import versions
 
 
 class HomepageView(TemplateView):
@@ -7,6 +12,36 @@ class HomepageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['test'] = "Hello World"
-        return context
-        
+
+        all_versions = versions.fetch_regulations_and_future_versions()
+        regs = self.get_regulations_list(all_versions)
+
+        c = {
+            'regulations': regs,
+        }
+
+        return {**context, **c}
+
+    def get_regulations_list(self, all_versions):
+        regs = []
+        reg_parts = sorted(all_versions.keys(), key=utils.make_sortable)
+
+        for part in reg_parts:
+            version = all_versions[part][0]['version']
+            reg_meta = utils.regulation_meta(part, version)
+            first_section = utils.first_section(part, version)
+            amendments = self.filter_future_amendments(all_versions.get(part, None))
+
+            reg = {'part': part,
+                'meta': reg_meta,
+                'reg_first_section': first_section,
+                'amendments': amendments}
+
+            regs.append(reg)
+        return regs
+
+    def filter_future_amendments(self, versions):
+        today = datetime.today()
+        amendments = [v for v in versions if v['by_date'] > today]
+        amendments.sort(key=lambda v: v['by_date'])
+        return amendments
