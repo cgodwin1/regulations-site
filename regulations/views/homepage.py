88712ec09250fce22c285054f324b7f0556e9f43
whytheplatypus <whytheplatypus@gmail.com>
2021-04-29 14:03:04 -0400

    Update homepage to use the "structure" of the parts

--- regulations/views/homepage.py
+++ regulations/views/homepage.py
@@ -13,43 +13,6 @@ logger = logging.getLogger(__name__)
 client = api_reader.ApiReader()
 
 
-def get_regulations_list(all_versions):
-    regs = []
-    reg_parts = sorted(all_versions.keys(), key=utils.make_sortable)
-
-    for part in reg_parts:
-        version = get_current_version(all_versions[part])
-        if version is not None:
-            reg_meta = utils.regulation_meta(part, version)
-            first_section = utils.first_section(part, version)
-            amendments = filter_future_amendments(all_versions.get(part, None))
-
-            reg = {
-                'part': part,
-                'meta': reg_meta,
-                'reg_first_section': first_section,
-                'amendments': amendments
-            }
-
-            regs.append(reg)
-    return regs
-
-
-def get_current_version(versions):
-    today = datetime.today()
-    for version in versions:
-        if version['by_date'] <= today:
-            return version['version']
-    return None
-
-
-def filter_future_amendments(versions):
-    today = datetime.today()
-    amendments = [v for v in versions if v['by_date'] > today]
-    amendments.sort(key=lambda v: v['by_date'])
-    return amendments
-
-
 class HomepageView(TemplateView):
 
     template_name = 'regulations/homepage.html'
